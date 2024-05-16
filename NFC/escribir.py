ad3e09a76b6636dafde5acf748ea1c13ec6bd653
ercid4 <167790699+ercid4@users.noreply.github.com>
2024-05-16 18:10:43 +0200

    escribir.py

--- NFC/escribir.py
+++ NFC/escribir.py
@@ -13,10 +13,13 @@ def write_data_to_card():
     while uid is None:
         uid = pn532.read_passive_target(timeout=0.5)
     print('Found card with UID:', [hex(i) for i in uid])
-    data = bytearray(b'Hola' + b'\x00' * 12)  # Datos a escribir
-    if pn532.mifare_classic_authenticate_block(uid, 4, 0x61, [0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF]):
-        if pn532.mifare_classic_write_block(4, data):
-            print('Data written to card')
+    text = input('Please enter the text to write to the card: ')
+    blocks = [text[i:i+16].ljust(16, '\x00') for i in range(0, len(text), 16)]  # Divide el texto en bloques de 16 bytes
+    for i, block in enumerate(blocks):
+        data = bytearray(block.encode('utf-8'))  # Datos a escribir
+        if pn532.mifare_classic_authenticate_block(uid, 4+i, 0x61, [0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF]):
+            if pn532.mifare_classic_write_block(4+i, data):
+                print('Data written to card')
 
 if __name__ == '__main__':
     setup()
