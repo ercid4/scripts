7411918c7c77b02cd17065e26412229d5b33d44a
ercid4 <167790699+ercid4@users.noreply.github.com>
2024-05-16 20:55:31 +0200

    firewall(no_socat).sh

---
+++ Linux/firewall(no_socat).sh
@@ -0,0 +1,10 @@
+sudo iptables -P INPUT DROP
+sudo iptables -P FORWARD DROP
+sudo iptables -P OUTPUT ACCEPT
+sudo iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+sudo iptables -A INPUT -i lo -j ACCEPT
+sudo iptables -A INPUT -p tcp --dport 22 -j ACCEPT
+sudo iptables -A INPUT -p tcp --dport 80 -j ACCEPT
+sudo iptables -A INPUT -p tcp --dport 443 -j ACCEPT
+sudo iptables-save > /etc/iptables/rules.v4
+nfc@nfc-server:~$ 
