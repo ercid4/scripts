bd1231544abc60a89e36c403590b739032952656
ercid4 <167790699+ercid4@users.noreply.github.com>
2024-05-16 20:57:33 +0200

    firewall(no_socat).sh

--- Linux/firewall(no_socat).sh
+++ Linux/firewall(no_socat).sh
@@ -7,4 +7,3 @@ sudo iptables -A INPUT -p tcp --dport 22 -j ACCEPT
 sudo iptables -A INPUT -p tcp --dport 80 -j ACCEPT
 sudo iptables -A INPUT -p tcp --dport 443 -j ACCEPT
 sudo iptables-save > /etc/iptables/rules.v4
-nfc@nfc-server:~$ 
